f1a8571924bc2bf58bc797801370b6a7fe3d090d
Дмитрий <selidimail@gmail.com>
2020-12-27 13:27:51 +0300

    Update settings.py

--- itreactor/settings.py
+++ itreactor/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p*vfv8&o&k!7zky8*-xi)$oay)v5@!1k=-!me&_%jzw1gqfvbp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
